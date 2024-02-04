ce4f528f2c0dc58bd8e758d9a7d7d93f9850ec1e
Ivan Moshak <ivanmoshak11@gmail.com>
2024-02-04 20:19:24 +0200

    css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,16 @@
 import './FriendListItem.css';
+import clsx from 'clsx';
 
 const FriendListItem = ({ friend: { avatar, name, isOnline }}) => {
+    const statusClasses = clsx("status", {
+        "online": isOnline,
+        "offline": !isOnline
+    });
     return (
         <div className="item">
             <img className="avatar" src={avatar} alt="Avatar" width="48" />
             <p className="name">{name}</p>
-            <p className="status">{isOnline ? 'Online ' : 'Offline'}</p>
+            <p className={statusClasses}>{isOnline ? 'Online ' : 'Offline'}</p>
         </div>
     );
 }
